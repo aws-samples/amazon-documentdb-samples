a4361563ae4034eede378fe850c86f2d60a69a19
Herbert Gomez <gomherbe@amazon.com>
2020-08-09 14:22:03 -0500

    Created a setup file for the AES targetç

---
+++ samples/change-streams/targets/elasticsearch-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create target dir and download AES sample template
+mkdir target
+cd target
+wget https://raw.githubusercontent.com/aws-samples/amazon-documentdb-samples/master/samples/change-streams/targets/elasticsearch-sample.json
+
+# Set ACCOUNT_ID and AWS_REGION
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | \
+  grep region | cut -d\" -f4)
+
+# Replace region and account
+replace awsregion "'$AWS_REGION'" -- elasticsearch-sample.json
+replace accountid "'$ACCOUNT_ID'" -- elasticsearch-sample.json 
