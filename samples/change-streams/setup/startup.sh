321eeec351e691baaae3ce6d74c886ade308546f
Herbert Gomez <gomherbe@amazon.com>
2020-08-12 16:40:17 -0500

    Automating step by creating lambda role automatically

--- samples/change-streams/setup/startup.sh
+++ samples/change-streams/setup/startup.sh
@@ -46,6 +46,30 @@ pip install aws-cdk.aws-lambda
 pip install aws-cdk.aws-sns
 pip install aws-cdk.aws-lambda-event-sources
 
+# Create SNS topics to be used in the solution
+cd ..
+export SNS_TRIGGER=$(aws sns create-topic --name sns_changestreams_trigger | jq -r .'TopicArn')
+export SNS_ALERT=$(aws sns create-topic --name sns_changestreams_alert | jq -r .'TopicArn')
+
+# Download Lambda rol template, replace variables, and create role
+wget 
+replace [SecretARN] "$DOCDB_CREDENTIALS" -- lambdaRole.json
+replace [TopicARN] "$SNS_TRIGGER" -- lambdaRole.json   
+export CS_POLICY=$(aws iam create-policy --policy-name policy-change-streams --policy-document file://lambdaRole.json | jq -r .'Policy'.'Arn')
+wget 
+export ROLE=$(aws iam create-role --role-name role-change-streams --assume-role-policy-document file://trustPolicy.json | jq -r .'Role'.'Arn') 
+aws iam attach-role-policy --policy-arn $CS_POLICY --role-name role-change-streams
+
+# Download config file for the CDK project
+cd change-streams-project
+wget https://raw.githubusercontent.com/aws-samples/amazon-documentdb-samples/master/samples/change-streams/cdk/config.ini
+replace [TopicARNAlert] "$SNS_ALERT" -- config.ini
+replace [TopicARNTrigger] "$SNS_TRIGGER" -- config.ini
+replace [ARNRole] "$ROLE" -- config.ini
+
+# Replace code of template CDK project with the one in GitHub
+wget https://raw.githubusercontent.com/aws-samples/amazon-documentdb-samples/master/samples/change-streams/cdk/change_streams_project_stack.py -O change_streams_project/change_streams_project_stack.py
+
 # Upload Lambda Code
 mkdir app
 cd ../app
@@ -60,4 +84,4 @@ pip install -r requirements.txt
 deactivate
 mv ../dist-packages/* .
 zip -r9 repLambdaFunction.zip .
-aws s3 cp repLambdaFunction.zip $S3_BUCKET
+aws s3 cp repLambdaFunction.zip s3://$S3_BUCKET
