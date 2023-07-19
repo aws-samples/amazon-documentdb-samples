877d3f2eac37d6875ba106d6387f44d2f71946d4
Mihai Aldoiu <maldoiu@amazon.com>
2023-07-19 17:56:01 +0100

    Add sample Lambda functions for Amazon DocumentDB

---
+++ lambda-samples/samples/docdb-killLongOp.py
@@ -0,0 +1,83 @@
+import os
+import pymongo
+import boto3
+import json
+import logging
+
+# Set up the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Set these Environment variables in Lambda Configuration
+DOCDB_SECRET_NAME = os.environ['DOCDB_SECRET_NAME']  
+THRESHOLD_SECONDS = int(os.environ['THRESHOLD_SECONDS']) 
+
+def get_credentials(secret_name):
+    session = boto3.session.Session()
+
+    try:
+        logger.info('Retrieving secret {} from Secrets Manger.'.format(secret_name))
+        client = session.client(service_name='secretsmanager', region_name=session.region_name)
+        secret_value = client.get_secret_value(SecretId=secret_name)
+        secret_json = json.loads(secret_value['SecretString'])
+        username = secret_json['username']
+        password = secret_json['password']
+        cluster_uri = secret_json['host']
+        logger.info('Secret {} retrieved from Secrets Manger.'.format(secret_name))
+        return (username, password, cluster_uri)
+    except Exception as e:
+        logger.error('Failed to retrieve secret {} because: {}'.format(secret_name, e))
+
+def kill_long_running_operations():
+    try:
+        # Retrieve DocumentDB credentials and cluster uri from AWS Secrets Manager
+        secret_name = os.environ['DOCDB_SECRET_NAME']  # Environment variable for secret name
+        (secret_username, secret_password, cluster_uri) = get_credentials(secret_name)
+
+        # Connect to Amazon DocumentDB
+        logger.info('Creating new DocumentDB client.')
+        client = pymongo.MongoClient(
+            cluster_uri,
+            tls=True,
+            retryWrites=False,
+            tlsCAFile='/opt/python/rds-combined-ca-bundle.pem',
+            username=secret_username,
+            password=secret_password,
+            authSource='admin')
+        logger.info('Successfully created new DocumentDB client.')
+
+        # Get current operations and filter long-running operations
+        db = client.admin # Use the admin database to run the currentOp command
+        with db.aggregate([
+            {"$currentOp": {}},
+            {"$match": {"secs_running": {"$gt": THRESHOLD_SECONDS}}}
+        ]) as cursor:
+            # Initialize a flag to check if operations were found over the threshold
+            operations_found = False
+            
+            # Iterate through the long-running operations and kill them
+            for op in cursor:
+                operations_found = True
+                op_id = op["opid"]
+                secs_running = op["secs_running"]
+                command = op["command"]
+                logger.info(f"Killing operation with ID {op_id} (running for {secs_running} seconds)")
+                logger.info(f"Command: {command}")
+                try:
+                    # Try to kill the operation
+                    db.command("killOp", op=op_id)
+                except pymongo.errors.OperationFailure as e:
+                    logger.error(f"Failed to kill operation with ID {op_id}: {str(e)}")
+
+            # If no operations were found over the threshold, log a message
+            if not operations_found:
+                logger.info(f"No operations found over the threshold.")
+
+        client.close()
+
+    except Exception as e:
+        logger.error("An error occurred:")
+        logger.error(str(e))
+
+def lambda_handler(event, context):
+    kill_long_running_operations()
---
+++ lambda-samples/samples/docdb-stopStart.py
@@ -0,0 +1,56 @@
+import boto3
+import os
+
+# REMINDER: Set the following environment variable depending on which action the function should take
+# ACTION = stop | start  
+
+docdb_client = boto3.client('docdb')
+
+def check_and_manage_docdb():
+    action = os.environ.get('ACTION', 'stop').lower()
+
+    if action == 'stop':
+        stop_docdb_clusters()
+    elif action == 'start':
+        start_docdb_clusters()
+    else:
+        print(f"Invalid action '{action}'. Valid options are 'stop' or 'start'.")
+
+def stop_docdb_clusters():
+    response = docdb_client.describe_db_clusters()
+    for cluster in response["DBClusters"]:
+        cluster_id = cluster["DBClusterIdentifier"]
+        cluster_arn = cluster["DBClusterArn"]
+        cluster_status = cluster["Status"]
+        tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
+        if cluster_status == 'available':
+            # Get tags to stop only clusters with the AllowStop tag
+            for item in tags['TagList']:
+                if item['Key'] == 'AllowStop' and item['Value'] == 'true':
+                    print(f"{cluster_arn} is {cluster_status} and AllowStop is {item['Value']}")
+                    print(f"Stopping cluster: {cluster_arn}")
+                    stop_docdb = docdb_client.stop_db_cluster(DBClusterIdentifier=cluster_id)
+                    print(stop_docdb)
+        else:
+            print(f"DocDB cluster {cluster_id} is not eligible to stop, set the AllowStop tag accordingly!")
+
+def start_docdb_clusters():
+    response = docdb_client.describe_db_clusters()
+    for cluster in response["DBClusters"]:
+        cluster_id = cluster["DBClusterIdentifier"]
+        cluster_arn = cluster["DBClusterArn"]
+        cluster_status = cluster["Status"]
+        tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
+        if cluster_status == 'stopped':
+            # Get tags to start only clusters with the AllowStart tag
+            for item in tags['TagList']:
+                if item['Key'] == 'AllowStart' and item['Value'] == 'true':
+                    print(f"{cluster_arn} is {cluster_status} and AllowStart is {item['Value']}")
+                    print(f"Starting cluster: {cluster_arn}")
+                    start_docdb = docdb_client.start_db_cluster(DBClusterIdentifier=cluster_id)
+                    print(start_docdb)
+        else:
+            print(f"DocDB cluster {cluster_id} is not eligible to start, set the AllowStart tag accordingly!")
+
+def lambda_handler(event, context):
+    check_and_manage_docdb()
