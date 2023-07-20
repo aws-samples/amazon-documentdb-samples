44f0a6e86a22b9288777d993acdc38ea08c494fa
Mihai Aldoiu <maldoiu@amazon.com>
2023-07-20 17:59:35 +0100

    Add sample Lambda functions for Amazon DocumentDB

--- lambda-samples/samples/docdb-stopStart.py
+++ lambda-samples/samples/docdb-stopStart.py
@@ -1,5 +1,10 @@
 import boto3
 import os
+import logging
+
+# Set up the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 ## Set the following environment variable depending on which action the function should take
 # ACTION = stop | start  
@@ -14,43 +19,64 @@ def check_and_manage_docdb():
     elif action == 'start':
         start_docdb_clusters()
     else:
-        print(f"Invalid action '{action}'. Valid options are 'stop' or 'start'.")
+        logger.error(f"Invalid action '{action}'. Valid options are 'stop' or 'start'.")
+
 
 def stop_docdb_clusters():
-    response = docdb_client.describe_db_clusters()
-    for cluster in response["DBClusters"]:
-        cluster_id = cluster["DBClusterIdentifier"]
-        cluster_arn = cluster["DBClusterArn"]
-        cluster_status = cluster["Status"]
-        tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
-        if cluster_status == 'available':
-            # Get tags to stop only clusters with the AllowStop tag
+    try:
+        response = docdb_client.describe_db_clusters()
+        for cluster in response["DBClusters"]:
+            cluster_id = cluster["DBClusterIdentifier"]
+            cluster_arn = cluster["DBClusterArn"]
+            cluster_status = cluster["Status"]
+            tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
+
+            # Flag to track if the cluster is eligible to stop
+            eligible_to_stop = False
+            # Check tags for AllowStop
             for item in tags['TagList']:
                 if item['Key'] == 'AllowStop' and item['Value'] == 'true':
-                    print(f"{cluster_arn} is {cluster_status} and AllowStop is {item['Value']}")
-                    print(f"Stopping cluster: {cluster_arn}")
-                    stop_docdb = docdb_client.stop_db_cluster(DBClusterIdentifier=cluster_id)
-                    print(stop_docdb)
-        else:
-            print(f"DocDB cluster {cluster_id} is not eligible to stop, set the AllowStop tag accordingly!")
+                    eligible_to_stop = True
+                    break
+
+            if cluster_status == 'available' and eligible_to_stop:
+                logger.info(f"{cluster_arn} is {cluster_status} and AllowStop tag is set to 'true'")
+                logger.info(f"Stopping cluster: {cluster_arn}")
+                stop_docdb = docdb_client.stop_db_cluster(DBClusterIdentifier=cluster_id)
+                logger.info(stop_docdb)
+            else:
+                logger.debug(f"DocDB cluster {cluster_id} is {cluster_status}, but not eligible to stop, set the AllowStop tag accordingly.")
+    except Exception as e:
+        logger.error(f"An error occurred while stopping clusters: {str(e)}")
+
 
 def start_docdb_clusters():
-    response = docdb_client.describe_db_clusters()
-    for cluster in response["DBClusters"]:
-        cluster_id = cluster["DBClusterIdentifier"]
-        cluster_arn = cluster["DBClusterArn"]
-        cluster_status = cluster["Status"]
-        tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
-        if cluster_status == 'stopped':
-            # Get tags to start only clusters with the AllowStart tag
+    try:
+        response = docdb_client.describe_db_clusters()
+        for cluster in response["DBClusters"]:
+            cluster_id = cluster["DBClusterIdentifier"]
+            cluster_arn = cluster["DBClusterArn"]
+            cluster_status = cluster["Status"]
+            tags = docdb_client.list_tags_for_resource(ResourceName=cluster_arn)
+            
+            # Flag to track if the cluster is eligible to start
+            eligible_to_start = False
+            # Check tags for AllowStart
             for item in tags['TagList']:
                 if item['Key'] == 'AllowStart' and item['Value'] == 'true':
-                    print(f"{cluster_arn} is {cluster_status} and AllowStart is {item['Value']}")
-                    print(f"Starting cluster: {cluster_arn}")
-                    start_docdb = docdb_client.start_db_cluster(DBClusterIdentifier=cluster_id)
-                    print(start_docdb)
-        else:
-            print(f"DocDB cluster {cluster_id} is not eligible to start, set the AllowStart tag accordingly!")
+                    eligible_to_start = True
+                    break
+            
+            if cluster_status == 'stopped' and eligible_to_start:
+                logger.info(f"{cluster_arn} is {cluster_status} and AllowStart tag is set to 'true'")
+                logger.info(f"Starting cluster: {cluster_arn}")
+                start_docdb = docdb_client.start_db_cluster(DBClusterIdentifier=cluster_id)
+                logger.info(start_docdb)
+            else:
+                logger.debug(f"DocDB cluster {cluster_id} is {cluster_status}, but not eligible to start, set the AllowStart tag accordingly.")
+    except Exception as e:
+        logger.error(f"An error occurred while starting clusters: {str(e)}")            
+
 
 def lambda_handler(event, context):
     check_and_manage_docdb()
