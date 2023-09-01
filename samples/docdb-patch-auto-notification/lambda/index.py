83597042df0405a28ee6a34b09eaeed012080962
Bingbing Liu <50776512+bingbingliu18@users.noreply.github.com>
2023-09-01 13:35:21 +0800

    Update index.py

--- samples/docdb-patch-auto-notification/lambda/index.py
+++ samples/docdb-patch-auto-notification/lambda/index.py
@@ -2,62 +2,59 @@ import boto3
 import json
 import os
 
-action_list=['system-update']
-notification = "This is notification for DocumentDB patch \n"
+service_list = ['DocumentDB']
+action_list = ['system-update']
+notification = f"This is a notification for {service_list[0]} pending maintenance.\n\n\n\n"
+
 
 def query_docdb():
     global notification
     EC2 = boto3.client('ec2')
     regions = EC2.describe_regions()
-   
+
     for REGION in regions['Regions']:
-        docDB = boto3.client('docdb',REGION['RegionName'])
+        docDB = boto3.client('docdb', REGION['RegionName'])
         pma = docDB.describe_pending_maintenance_actions()
         if len(pma['PendingMaintenanceActions']) > 0:
             num = len(pma['PendingMaintenanceActions'])
             for inst in pma['PendingMaintenanceActions']:
-                mw_description=inst['PendingMaintenanceActionDetails'][0]['Description']
-                mw_action=inst['PendingMaintenanceActionDetails'][0]['Action']
+                print(f"instance {inst}")
+                mw_description = inst['PendingMaintenanceActionDetails'][0]['Description']
+                mw_action = inst['PendingMaintenanceActionDetails'][0]['Action']
                 if mw_action not in action_list:
                     num = num - 1
                     continue
                 if not pending_action_is_docdb_cluster_engine_patch(docDB, inst):
                     num = num - 1
-                    continue    
-                temp_string = "**********************************************" +  "\n"
-                notification += temp_string
-                temp_string = "Region: " + REGION['RegionName'] +  " has  docDB cluster needed to be upgrade.\n"
-                notification += temp_string
-                temp_string = "Mw_description: " + mw_description +  "\n"
-                notification += temp_string
-                temp_string = "Mw_action: " + mw_action +  "\n"
-                notification += temp_string
-                temp_string = "The Resource has pending maintenance action: " + inst['ResourceIdentifier'] + "\n"
-                notification += temp_string
-                cls_identifier=inst['ResourceIdentifier'].split(':')[-1]
+                    continue
+                notification += f"-----\n"
+                notification += f"Region: {REGION['RegionName']} has a {service_list[0]} cluster that has pending maintenance action\n"
+                notification += f"Maintenance Window Description: {mw_description}\n"
+                notification += f"Maintenance Window Action: {mw_action}\n"
+                notification += f"Resource with pending maintenance action: {inst['ResourceIdentifier']}\n"
+                cls_identifier = inst['ResourceIdentifier'].split(':')[-1]
                 try:
-                    cls = docDB.describe_db_clusters(DBClusterIdentifier=cls_identifier)
+                    cls = docDB.describe_db_clusters(
+                        DBClusterIdentifier=cls_identifier)
                 except Exception as e:
-                        temp_string = "Failed to get cluster information! This is instance level notification" + "\n" 
-                        notification += temp_string
-                        temp_string = "**********************************************" +  "\n"
-                        notification += temp_string
-                        continue
-                dbclus=cls['DBClusters']
+                    notification += "Failed to get cluster information! This is instance level notification\n"
+                    notification += f"-----\n"
+                    continue
+                dbclus = cls['DBClusters']
                 mw = dbclus[0]['PreferredMaintenanceWindow']
-                temp_string = "The Cluster maintenance window UTC time: " + mw + "\n"
-                notification += temp_string
-            temp_string = "Region: " + REGION['RegionName'] + " has total " + str(num) + " resource found.\n"   
-            notification += temp_string
-            temp_string = "**********************************************" +  "\n"
-            notification += temp_string
+                notification += f"The Cluster maintenance window UTC time: {mw}\n"
+                notification += f"Region: {REGION['RegionName']} has a total of {num} with pending maintenance action.\n"
+                notification += f"-----\n"
         else:
-            temp_string = "Region: " + REGION['RegionName'] + " No Cluster found.\n"
-            notification += temp_string
+            notification += f"-----\n"
+            notification += f"Region: {REGION['RegionName']} has no {service_list[0]} cluster with pending maintenance.\n"
+            notification += f"-----\n"
+
+
 def pending_action_is_docdb_cluster_engine_patch(docdb_client, pending_action):
     """
     For a pending maintenance action to be a DocumentDB cluster engine patch, ALL of the following MUST be true:
-    
+
     1. The ARN of the pending maintenance action is for a 'cluster'
         * Ex. arn = "arn:aws:rds:us-east-1:000123456789:cluster:test-cluster-1"
     2. The 'action' of a pending maintenance action must be type 'system-update'
@@ -69,26 +66,29 @@ def pending_action_is_docdb_cluster_engine_patch(docdb_client, pending_action):
     if pending_action_resource_arn.split(":")[5] != "cluster":
         return False
     # 2
-    action_type = pending_action['PendingMaintenanceActionDetails'][0].get('Action')
+    action_type = pending_action['PendingMaintenanceActionDetails'][0].get(
+        'Action')
     if action_type != "system-update":
         return False
-    # 3 
+    # 3
     cluster_identifier = pending_action_resource_arn.split(":")[6]
-    describe_db_cluster_response = docdb_client.describe_db_clusters(DBClusterIdentifier=cluster_identifier)
+    describe_db_cluster_response = docdb_client.describe_db_clusters(
+        DBClusterIdentifier=cluster_identifier)
     if describe_db_cluster_response["DBClusters"][0]["Engine"] != "docdb":
         return False
-    return True     
+    return True
+
 
 def lambda_handler(event, context):
     global notification
     sns_client = boto3.client('sns')
     query_docdb()
-    print (notification)
+    print(notification)
     sns_arn = os.environ.get('SNS_TOPIC_ARN')
 
-    sns_response = sns_client.publish (
-        TargetArn = sns_arn,
-        Subject = "Database Patch Notification",
-        Message = json.dumps({'default': notification}),
-        MessageStructure = 'json'
-     )
+    sns_response = sns_client.publish(
+        TargetArn=sns_arn,
+        Subject=f"AWS {service_list[0]} Patch Notification",
+        Message=json.dumps({'default': notification}),
+        MessageStructure='json'
+    )
