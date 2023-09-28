d33e3d008f6a02262c4f5ae5a38f63f6da9f3857
sourav biswas <siswasb@amazon.com>
2023-09-28 08:25:17 -0600

    document elasticache blog code snippets

---
+++ blogs/docdb-elasticache/setup_lab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DOCDB_HOST=<your docdb cluster endpoint>
+export DOCDB_DB="better_together"
+export DOCDB_COL="test_data"
+export DOCDB_USER="docdbadmin"
+export DOCDB_PASS="<password."
+export DOCDB_PORT=27017
+
+export ELASTICACHE_ENDPOINT=<your elasticache endpoint>
+export ELASTICACHE_PORT=6379
