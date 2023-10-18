be5a1ec9c500a03d086c90a3b76b12d3db0533a2
Anshu Vajpayee <anshu.vajpayee@gmail.com>
2023-10-18 11:13:11 +0000

    modifying tool as per feedback

--- samples/docdb-scaling/README.md
+++ samples/docdb-scaling/README.md
@@ -8,21 +8,28 @@ Tool performs scaling operations in three simple steps as below and it does not 
  - Remove old instances.
 
 # Requirements
- - Python 3.7+
+ - Python 3.8+
  - pymongo Python package - tested versions
    - pymongo 4.4+
    - If not installed - "$ pip3 install pymongo"
- - AWS SDK for Python - tested versions 
+ - AWS SDK for Pythoni(Boto3) - tested versions 
    - boto3 1.28+
    - If not installed - "$ pip3 install boto3"
+ - Configure Boto3 credentials
+   - Configure AWS IAM role to access Amazon DocumentDB and assign to Amazon EC2 instance. 
+   - There are multiple ways to configure Boto3 credentials. You can refer here : https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+   - Recommnedation is to use AWS IAM role if you are running tool from Amazon EC2.
 
 
 ## Using the Scale Up/Down Tool
 `python3 scaleupdown.py  --cluster-id <cluster-id> --target-instance-type <target-instance-type>  `
-- Run on any AWS Cloud9 or Bastion host(Jump host) where aws cli is configured.
 - Provide a valid cluster id - Mandatory
 - Pass a valid instance type - Mandatory
 - Ignore the check for existing instances for target instance type using --ignore-instance-check (Optional)
 
+## Sample command
+```python
+python3 scaleupdown.py --cluster-id source-docdb-3-6-cluster --target-instance-type db.r5.medium
+```
 ## License
 This tool is licensed under the Apache 2.0 License. 
--- samples/docdb-scaling/scaleupdown.py
+++ samples/docdb-scaling/scaleupdown.py
@@ -1,27 +1,17 @@
 import boto3
-import json
-import os
 import time
 import logging
 import sys
 import argparse,traceback
 
+
 logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S',level=logging.INFO)
 LOGGER = logging.getLogger()
 docdb = boto3.client('docdb')
 
-
-def isClusterStatusAvailable(cluster_id):
-    r = docdb.describe_db_clusters( DBClusterIdentifier=cluster_id)
-    cluster_info = r['DBClusters'][0]
-    if cluster_info['Status'] != 'available':
-        return False
-    return True
-    
+#Function to get  instance count  with status - available, failed and pending   
 def getInstanceStatus(cluster_id):
-    if not isClusterStatusAvailable(cluster_id):
-        return 
-    istatus={}
+    istatus={"num_instances":-1}
     num_available = 0
     num_pending = 0
     num_failed = 0
@@ -41,41 +31,45 @@ def getInstanceStatus(cluster_id):
     istatus["num_pending"]=num_pending
     istatus["num_failed"]=num_failed
     return istatus
-    
-def getClusterInstances(cluster_id):
+
+#Function to get cluster health 
+def getClusterHealth(cluster_id):
     r = docdb.describe_db_clusters( DBClusterIdentifier=cluster_id)
     cluster_info = r['DBClusters'][0]
-    if cluster_info['Status'] != 'available':
-        return False
-    else:
-        existing_instances = {}
-        for member in cluster_info['DBClusterMembers']:
+    istatus=getInstanceStatus(cluster_id)
+    if cluster_info['Status'] == 'available' and istatus["num_available"]==istatus["num_instances"] :
+        return True
+    return False
+
+
+#Function to get cluster instances and role 
+def getClusterInstances(cluster_id):
+    r = docdb.describe_db_clusters( DBClusterIdentifier=cluster_id)
+    existing_instances = {}
+    cluster_info = r['DBClusters'][0]
+    if cluster_info['Status'] == 'available':
+       for member in cluster_info['DBClusterMembers']:
             member_id = member['DBInstanceIdentifier']
             if member['IsClusterWriter']==True:
                 existing_instances[member_id]='WRITER'
             else:
                 existing_instances[member_id]='READER'
-    
     return existing_instances
 
+#Function to get writer instance for a given cluster id 
 def getWriterInstance(cluster_id):
     instances=getClusterInstances(cluster_id=cluster_id)
+    if len(instances)==0:
+        raise RuntimeError("Not able to retrive primary instance") 
     reversed_dict = {}
     for key, value in instances.items():
         reversed_dict.setdefault(value, [])
         reversed_dict[value].append(key)
     return reversed_dict["WRITER"][0]
-
-def isClusterHealthy(cluster_id):
-    istatus=getInstanceStatus(cluster_id)
-    LOGGER.info("cluster instance status: " + str(istatus))
-    if istatus==None:
-        LOGGER.error("Make sure cluster is in started state")
-        return False
-    if istatus["num_available"]==istatus["num_instances"] and isClusterStatusAvailable(cluster_id) :
-        return True
-    return False
     
+
+
+#Function to add the number of instances with size on a given cluster id     
 def addInstances(cluster_id,desired_size,count):
     ninstances=[]
     for idx in range(count):
@@ -84,14 +78,16 @@ def addInstances(cluster_id,desired_size,count):
         ninstances.append(instance_iden)
     return ninstances
         
-
+#Function to delete the instances from instancelist 
 def deleteInstances(instanceIdList):
     for instance in instanceIdList:
         docdb.delete_db_instance(DBInstanceIdentifier=instance)
-        
-def perfromFailover(cluster_id,failover_target):
+
+#Function to perfrom failover using failover target         
+def performFailover(cluster_id,failover_target):
     docdb.failover_db_cluster(DBClusterIdentifier=cluster_id,TargetDBInstanceIdentifier=failover_target)
     
+#Function to check existing  instance class  
 def checkExistingInstanceClass(cluster_id,itarget):
     r = docdb.describe_db_instances(Filters=[{'Name':'db-cluster-id','Values': [cluster_id]}])
     instances = r['DBInstances']
@@ -103,16 +99,11 @@ def checkExistingInstanceClass(cluster_id,itarget):
     else:
         return False
 
-def checkClusterHealth(cluster_id,mins=15):
-    count=0
-    while not isClusterHealthy(cluster_id=cluster_id):
-        count=count+1
-        if count==mins:
-            LOGGER.error("Cluster status is not healthy after " + str(mins) +  " mins. Please check the cluster health .Exiting.. ")
-            return False
-        LOGGER.info("will be checking cluster health in 60 seconds")
-        time.sleep(60)
-    return True
+#DocDB instance available waiter to check isntance status 
+def docdbDBInstanceAvailableWaiter(cluster_id):
+    waiter = docdb.get_waiter('db_instance_available')
+    waiter.wait(Filters=[{ 'Name': 'db-cluster-id','Values': [cluster_id]}])
+
 
 def main():
     
@@ -136,69 +127,103 @@ def main():
 
     cluster_id=args.cluster_id
     itarget=args.target_instance_type
-    monitor_deletion=False
     target_instance_check=args.ignore_instance_check
     valid_instance_classes=["db.r6g.large","db.r6g.xlarge","db.r6g.2xlarge","db.r6g.4xlarge","db.r6g.8xlarge","db.r6g.12xlarge","db.r6g.16xlarge","db.r5.large","db.r5.xlarge","db.r5.2xlarge","db.r5.4xlarge","db.r5.8xlarge","db.r5.12xlarge","db.r5.16xlarge","db.r5.24xlarge","db.t4g.medium","db.t3.medium"]
-   
+
+    #checking if the target instance class is valid.    
     if itarget not in valid_instance_classes:
         LOGGER.error("Target instance type "+ itarget +" is invalid. Exiting..")
         sys.exit(1)
-   
-    if target_instance_check and checkExistingInstanceClass(cluster_id=cluster_id,itarget=itarget):
-        LOGGER.error("Cluster already has one of the instance with target instance type.If you want to still continue,please run with --ignore-instance-check.Exiting..")
+    
+    # Check for for cluster id and cluster health
+    try:
+        clusterHealth=getClusterHealth(cluster_id=cluster_id)
+    except Exception as e :
+        LOGGER.error(str(e))
+        LOGGER.error("Please check the cluster id and try again. Exiting..")
         sys.exit(1)
 
-    if not isClusterHealthy(cluster_id):
-        LOGGER.error("Cluster is not healthy. Exiting..")
+    if not clusterHealth:
+        LOGGER.error("Cluster is not healthy. Please make sure that cluster is in healthy state before running the tool. Exiting..")
         sys.exit(1)
-        return False 
+    
+    if target_instance_check and checkExistingInstanceClass(cluster_id=cluster_id,itarget=itarget):
+        LOGGER.error("Cluster already has one of the instance with target instance type. If you want to still continue, please run with --ignore-instance-check. Exiting..")
+        sys.exit(1)
+
     
     num_instances=getInstanceStatus(cluster_id)["num_instances"]
     oinstances=list((getClusterInstances(cluster_id)).keys())
     
     if num_instances > 8:
-        LOGGER.error("Number of instances can not be more than 8 for this script.")
-        sys.exit(1)
-        return False
-    
-    ## Add instances 
-    LOGGER.info("Adding "+ str(len(oinstances))+  " instances with instance type " + itarget)
-    ninstances=addInstances(cluster_id=cluster_id,desired_size=itarget,count=num_instances)
-    LOGGER.info("checking  cluster health after instance addition" )
-    count=0
-    if checkClusterHealth(cluster_id=cluster_id):
-        LOGGER.info("Cluster is healthy post instance addition,performing failover")
-    else:
-        LOGGER.info("Cluster is not healthy post instance addition,existing")
+        LOGGER.error("Number of instances can not be more than 8 for this tool.")
         sys.exit(1)
     
+    
+    LOGGER.info("Target  instance type " + itarget)
+    LOGGER.info("Number of instances to add  "+ str(len(oinstances))+", adding instances")
+    ## Add instances
+    try:
+        ninstances=addInstances(cluster_id=cluster_id,desired_size=itarget,count=num_instances)
+    except Exception as e :
+        LOGGER.error(str(e))
+        LOGGER.error("There is issue in adding instances,please fix the above exception and try again. Exiting..")
+        sys.exit(1)
+
+    LOGGER.info("waiting for new instances to become available, it can take up to 15 mins" )
+    
+    try:
+        docdbDBInstanceAvailableWaiter(cluster_id)
+    except Exception as e :
+        LOGGER.error(str(e))
+        LOGGER.error("New instances are not healthy after 15 mins. Exiting..")
+        sys.exit(1)
+    
+
+    #Get failover target and current writer
     failover_target=ninstances[0]
-    old_writer=getWriterInstance(cluster_id=cluster_id)
-    LOGGER.info("Failover target - " + failover_target)
+    current_writer=getWriterInstance(cluster_id=cluster_id)
+
+
+    LOGGER.info("Failover target " + failover_target)
     
     ## Perform failover 
-    perfromFailover(cluster_id=cluster_id,failover_target=failover_target)
+    LOGGER.info("performing failover")
+    try:
+        performFailover(cluster_id=cluster_id,failover_target=failover_target)
+        time.sleep(30)
+    except Exception as e :
+        LOGGER.error(str(e))
+        LOGGER.error("There is issue with failover, please check the cluster health. Exiting..")
+        sys.exit(1)
+    count=0
     LOGGER.info("checking failover status")
-    for count in range(4):
-        nwriter=getWriterInstance(cluster_id=cluster_id)
-        if  nwriter in ninstances and old_writer!=nwriter:
+    for count in range(10):
+        try:
+            nwriter=getWriterInstance(cluster_id=cluster_id)
+        except Exception as e :
+            LOGGER.warning(str(e))
+            LOGGER.info("retrying to retrive new primary instance in 30 secs")
+            count=count+1
+            time.sleep(30)
+            continue  
+        if  nwriter in ninstances and current_writer!=nwriter:
+            LOGGER.info("failover completed, new primary is " + nwriter)
             break
-        LOGGER.info("sleeping for 30 sec to check failover status")
+        LOGGER.info("sleeping for 30 secs to check failover status")
+        count=count+1
         time.sleep(30)
 
-    ## Delete old instances 
-    LOGGER.info("deleting old instances.")
-    deleteInstances(oinstances)
-    if monitor_deletion and checkClusterHealth(cluster_id=cluster_id):
-        LOGGER.info("monitoring deleteion process")
-        LOGGER.info("Cluster is healthy post instance deletion")
-    elif monitor_deletion:
-        LOGGER.info("Cluster is not healthy post instance deletion")
-    LOGGER.info("scaling process completed.")
+    ## Delete  old instances 
+    LOGGER.info("deleting old instances")
+    try:
+        deleteInstances(oinstances)
+    except Exception as e :
+        LOGGER.error(str(e))
+        LOGGER.erbrror("There is issue with instance deletion, please check the cluster health. Exiting..")
+        sys.exit(1)
+
+    LOGGER.info("scaling tool execution finished.")
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        trc = traceback.format_exc()
-        LOGGER.error(str(e))
+    main()
