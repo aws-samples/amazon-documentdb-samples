688166bdb20ca01a941f9badeea4f56f24661e00
Tim Callaghan <tmcallaghan@gmail.com>
2024-01-12 18:31:41 +0000

    adding to samples repo

---
+++ samples/python-bench02/README.md
@@ -0,0 +1,41 @@
+# Python Insert Benchmark (bench02)
+This sample applications performs an insert-only workload on Amazon DocumentDB instance-based clusters and elastic clusters with several configurable parameters.
+
+## Requirements
+Python 3.6 or later
+pymongo
+
+## Installation
+Clone the repository and go to the application folder:
+```
+git clone https://github.com/aws-samples/amazon-documentdb-samples.git
+cd amazon-documentdb-samples/samples/python-bench02
+```
+
+## Usage/Examples
+The application has the following arguments:
+```
+Required parameters
+  --uri URI                                      URI (connection string)
+  --processes PROCESSES                          Degree of concurrency
+  --database DATABASE                            Database
+  --collection COLLECTION                        Collection
+  --run-seconds RUN_SECONDS                      Total number of seconds to run for
+  --batch-size BATCH_SIZE                        Number of documents to insert per batch
+
+Optional parameters
+  --rate-limit RATE_LIMIT                        Limit throughput (operations per second), default=9999999
+  --text-size TEXT_SIZE                          Size of text field (bytes), default=1024
+  --text-compressible TEXT_COMPRESSIBLE          Compressibility of text field (percentage), default=25
+  --ordered-batches                              Use ordered bulk-writes, default=unordered batches
+  --drop-collection                              Drop the collection (if it exists), default=do not drop
+  --compress                                     Compress the collection, default=no
+  --shard                                        Shard the collection, default=no
+  --num-customers NUM_CUSTOMERS                  Number of customers, default=10000
+  --num-products NUM_PRODUCTS                    Number of products, default=1000000
+  --max-quantity MAX_QUANTITY                    Maximum quantity, default=100000
+  --seconds-date-range SECONDS_DATE_RANGE        Number of seconds for range of orderDate field, default=90*87400
+  --num-secondary-indexes {0,1,2,3}              Number of secondary indexes, default=3
+  --file-name FILE_NAME                          Starting name of the created CSV and log files, default="bench02-output"
+```
+
---
+++ samples/python-bench02/bench02.py
@@ -0,0 +1,400 @@
+from datetime import datetime, timedelta
+import sys
+import random
+import json
+import bson
+import pymongo
+import time
+import threading
+import os
+import multiprocessing as mp
+import argparse
+import string
+
+
+def deleteLog(appConfig):
+    if os.path.exists(appConfig['logFileName']):
+        os.remove(appConfig['logFileName'])
+
+
+def printLog(thisMessage,appConfig):
+    print("{}".format(thisMessage))
+    with open(appConfig['logFileName'], 'a') as fp:
+        fp.write("{}\n".format(thisMessage))
+
+
+def deleteCsv(appConfig):
+    if os.path.exists(appConfig['csvFileName']):
+        os.remove(appConfig['csvFileName'])
+
+
+def printCsv(thisMessage,appConfig):
+    with open(appConfig['csvFileName'], 'a') as fp:
+        fp.write("{}\n".format(thisMessage))
+
+
+def reportCollectionInfo(appConfig):
+    client = pymongo.MongoClient(appConfig['uri'])
+    db = client[appConfig['databaseName']]
+    
+    collStats = db.command("collStats", appConfig['collectionName'])
+    
+    compressionRatio = collStats['size'] / collStats['storageSize']
+    gbDivisor = 1024*1024*1024
+    
+    printLog("collection statistics | numDocs             = {0:12,d}".format(collStats['count']),appConfig)
+    printLog("collection statistics | avgObjSize          = {0:12,d}".format(int(collStats['avgObjSize'])),appConfig)
+    printLog("collection statistics | size (GB)           = {0:12,.4f}".format(collStats['size']/gbDivisor),appConfig)
+    printLog("collection statistics | storageSize (GB)    = {0:12,.4f} ".format(collStats['storageSize']/gbDivisor),appConfig)
+    printLog("collection statistics | compressionRatio    = {0:12,.4f}".format(compressionRatio),appConfig)
+    printLog("collection statistics | totalIndexSize (GB) = {0:12,.4f}".format(collStats['totalIndexSize']/gbDivisor),appConfig)
+    
+    client.close()
+
+
+def setup(appConfig):
+    databaseName = appConfig['databaseName']
+    collectionName = appConfig['collectionName']
+    numSecondaryIndexes = appConfig['numSecondaryIndexes']
+
+    client = pymongo.MongoClient(appConfig['uri'])
+    db = client[databaseName]
+    col = db[collectionName]
+    nameSpace = "{}.{}".format(databaseName,collectionName)
+
+    if appConfig['dropCollection']:
+        printLog("Dropping collection {}".format(nameSpace),appConfig)
+        startTime = time.time()
+        col.drop()
+        elapsedMs = int((time.time() - startTime) * 1000)
+        printLog("  completed in {} ms".format(elapsedMs),appConfig)
+        
+        if appConfig['shard']:
+            # sharded
+            printLog("Creating sharded collection {}".format(nameSpace),appConfig)
+            startTime = time.time()
+            db.command({'shardCollection':nameSpace,'key':{'customerId':'hashed'}})
+            elapsedMs = int((time.time() - startTime) * 1000)
+            printLog("  completed in {} ms".format(elapsedMs),appConfig)
+        else:
+            # not sharded
+            if appConfig['compress']:
+                printLog("Creating compressed collection {}".format(nameSpace),appConfig)
+                startTime = time.time()
+                db.create_collection(name=collectionName,storageEngine={"documentDB":{"compression":{"enable":True}}})
+                elapsedMs = int((time.time() - startTime) * 1000)
+                printLog("  completed in {} ms".format(elapsedMs),appConfig)
+            else:
+                printLog("Creating uncompressed collection {}".format(nameSpace),appConfig)
+                startTime = time.time()
+                db.create_collection(name=collectionName)
+                elapsedMs = int((time.time() - startTime) * 1000)
+                printLog("  completed in {} ms".format(elapsedMs),appConfig)
+    
+        # create indexes
+        if numSecondaryIndexes >= 1:
+            thisIndexName = 'idx_customerId_orderDate'
+            printLog("Creating index {}".format(thisIndexName),appConfig)
+            col.create_index([('customerId', pymongo.ASCENDING),('orderDate', pymongo.ASCENDING)], name=thisIndexName, background=False, unique=False)
+
+        if numSecondaryIndexes >= 2:
+            thisIndexName = 'idx_customerId_productId'
+            printLog("Creating index {}".format(thisIndexName),appConfig)
+            col.create_index([('customerId', pymongo.ASCENDING),('productId', pymongo.ASCENDING)], name=thisIndexName, background=False, unique=False)
+
+        if numSecondaryIndexes >= 3:
+            thisIndexName = 'idx_productId_orderDate'
+            printLog("Creating index {}".format(thisIndexName),appConfig)
+            col.create_index([('productId', pymongo.ASCENDING),('orderDate', pymongo.ASCENDING)], name=thisIndexName, background=False, unique=False)
+
+    # get existing count of documents
+    numExistingDocuments = col.estimated_document_count()
+    if (numExistingDocuments > 0):
+        printLog("collection already contains {0} document(s)".format(numExistingDocuments),appConfig)
+
+    client.close()
+    
+    return numExistingDocuments
+
+
+def reporter(perfQ,appConfig):
+    numSecondsFeedback = 10
+    numIntervalsTps = 5
+    numInsertProcesses = appConfig['numInsertProcesses']
+    numExistingDocuments = appConfig['numExistingDocuments']
+
+    recentTps = []
+    
+    startTime = time.time()
+    lastTime = time.time()
+    numTotalInserts = 0
+    lastNumTotalInserts = 0
+    nextReportTime = startTime + numSecondsFeedback
+    intervalLatencyMs = 0
+    
+    numThreadsCompleted = 0
+    
+    csvHeader = "timestamp,elapsed-time,elapsed-seconds,inserts,overall-ips,total-documents,last-{}-intervals-ips,this-interval-ips,interval-latency-ms"
+    printCsv(csvHeader,appConfig)
+    
+    while (numThreadsCompleted < numInsertProcesses):
+        time.sleep(numSecondsFeedback)
+        nowTime = time.time()
+        
+        numLatencyBatches = 0
+        numLatencyMs = 0
+        
+        while not perfQ.empty():
+            qMessage = perfQ.get_nowait()
+            if qMessage['name'] == "batchCompleted":
+                numLatencyBatches += 1
+                numLatencyMs += qMessage['latency']
+                numTotalInserts += qMessage['inserts']
+            elif qMessage['name'] == "processCompleted":
+                numThreadsCompleted += 1
+
+        # total total
+        elapsedSeconds = nowTime - startTime
+        insertsPerSecond = numTotalInserts / elapsedSeconds
+
+        # elapsed hours, minutes, seconds
+        thisHours, rem = divmod(elapsedSeconds, 3600)
+        thisMinutes, thisSeconds = divmod(rem, 60)
+        thisHMS = "{:0>2}:{:0>2}:{:05.2f}".format(int(thisHours),int(thisMinutes),thisSeconds)
+        
+        # this interval
+        intervalElapsedSeconds = nowTime - lastTime
+        intervalInserts = numTotalInserts - lastNumTotalInserts
+        intervalInsertsPerSecond = intervalInserts / intervalElapsedSeconds
+        if numLatencyBatches > 0:
+            intervalLatencyMs = numLatencyMs // numLatencyBatches
+        else:
+            intervalLatencyMs = 0
+        
+        # recent intervals
+        if len(recentTps) == numIntervalsTps:
+            recentTps.pop(0)
+        recentTps.append(intervalInsertsPerSecond)
+        totRecentTps = 0
+        for thisTps in recentTps:
+            totRecentTps += thisTps
+        avgRecentTps = totRecentTps / len(recentTps)
+        
+        logTimeStamp = datetime.utcnow().isoformat()[:-3] + 'Z'
+        printLog("[{}] elapsed {} | total ins {:16,d} at {:12,.2f} p/s | tot docs {:16,d} | last {} {:12,.2f} p/s | interval {:12,.2f} p/s | lat (ms) {:12}".format(logTimeStamp,thisHMS,numTotalInserts,insertsPerSecond,numTotalInserts+numExistingDocuments,numIntervalsTps,avgRecentTps,intervalInsertsPerSecond,intervalLatencyMs),appConfig)
+        csvData = "{},{},{:.2f},{},{:.2f},{},{:.2f},{:.2f},{}".format(logTimeStamp,thisHMS,elapsedSeconds,numTotalInserts,insertsPerSecond,numTotalInserts+numExistingDocuments,avgRecentTps,intervalInsertsPerSecond,intervalLatencyMs)
+        printCsv(csvData,appConfig)
+        nextReportTime = nowTime + numSecondsFeedback
+        
+        lastTime = nowTime
+        lastNumTotalInserts = numTotalInserts
+
+
+def task_worker(threadNum,perfQ,appConfig):
+    rateLimit = appConfig['rateLimit']
+    runSeconds = appConfig['runSeconds']
+    numInsertsPerBatch = appConfig['batchSize']
+    numInsertProcesses = appConfig['numInsertProcesses']
+    numExistingDocuments = appConfig['numExistingDocuments']
+    textSize = appConfig['textSize']
+    pctCompressed = appConfig['textCompressible']
+    orderedBatches = appConfig['orderedBatches']
+    numCustomers = appConfig['numCustomers']
+    numProducts = appConfig['numProducts']
+    maxQuantity = appConfig['maxQuantity']
+    numSecondsDateRange = appConfig['secondsDateRange']
+
+    perfReportInterval = 1
+
+    rateLimitPerThread = rateLimit//numInsertProcesses
+
+    numBatchesCompleted = 0
+
+    client = pymongo.MongoClient(appConfig['uri'])
+    myDatabaseName = appConfig['databaseName']
+    db = client[myDatabaseName]
+    myCollectionName = appConfig['collectionName']
+    col = db[myCollectionName]
+
+    start = datetime(1980, 1, 1, 1, 1, 1)
+    end = datetime(2023, 9, 7, 1, 1, 1)
+    
+    textValue = "0123456789" * 100
+    randomChars = int(textSize * (1 - (pctCompressed / 100)))
+    fixedChars = textSize - randomChars
+    randomTextBufferSize = 4*1024*1024
+    randomTextBufferMaxStart = randomTextBufferSize - fixedChars
+    # generate a 4MB random string
+    randomString = ''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase,k=4*1024*1024))
+    fixedString = "a" * fixedChars
+
+    startTime = time.time()
+    nextPerfReportTime = time.time() + perfReportInterval
+    intervalSeconds = 2
+    nextReportTime = startTime + intervalSeconds
+    maxPerInterval = rateLimitPerThread * intervalSeconds
+
+    thisIntervalOps = 0
+    thisBatchInserts = 0
+    batchElapsedMs = 0
+    
+    allDone = False
+
+    while not allDone:
+        # check if we need to slow down
+        if time.time() > nextReportTime:
+            # we are running slower than the rate limiter
+            nextReportTime = time.time() + intervalSeconds
+            thisIntervalOps = 0
+        elif thisIntervalOps >= maxPerInterval:
+            # slow down
+            sleepTimeSeconds = nextReportTime - time.time()
+            #printLog("RATE LIMITING : sleeping for {:8,.3f} seconds".format(sleepTimeSeconds),appConfig)
+            time.sleep(sleepTimeSeconds)
+            nextReportTime = time.time() + intervalSeconds
+            thisIntervalOps = 0
+
+        insList = []
+        
+        for batchLoop in range(numInsertsPerBatch):
+            thisInsert = {}
+            
+            thisTimestamp = datetime.utcnow()
+            thisInsert["customerId"] = random.randint(1,numCustomers)
+            thisInsert["productId"] = random.randint(1,numProducts)
+            thisInsert["quantity"] = random.randint(1,maxQuantity)
+            thisInsert["orderDate"] = datetime.utcnow()-timedelta(seconds=numSecondsDateRange)
+
+            randomStringStart = random.randint(1,randomTextBufferMaxStart)-1
+            thisInsert["textField"] = randomString[randomStringStart:randomStringStart+randomChars]+fixedString
+            
+            insList.append(pymongo.InsertOne(thisInsert))
+            
+            thisBatchInserts += 1
+            thisIntervalOps += 1
+        
+        batchStartTime = time.time()
+        result = col.bulk_write(insList, ordered=orderedBatches)
+        batchElapsedMs += int((time.time() - batchStartTime) * 1000)
+        numBatchesCompleted += 1
+        
+        if time.time() > nextPerfReportTime:
+            nextPerfReportTime = time.time() + perfReportInterval
+            perfQ.put({"name":"batchCompleted","operations":numInsertsPerBatch*numBatchesCompleted,"latency":batchElapsedMs,"inserts":thisBatchInserts})
+            numBatchesCompleted = 0
+            batchElapsedMs = 0
+            thisBatchInserts = 0
+            
+        if (time.time() - startTime) >= runSeconds:
+            allDone = True
+        
+    client.close()
+    
+    perfQ.put({"name":"processCompleted","processNum":threadNum})
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Data Generator')
+
+    parser.add_argument('--uri',required=True,type=str,help='URI (connection string)')
+    parser.add_argument('--processes',required=True,type=int,help='Degree of concurrency')
+    parser.add_argument('--database',required=True,type=str,help='Database')
+    parser.add_argument('--collection',required=True,type=str,help='Collection')
+    parser.add_argument('--run-seconds',required=True,type=int,help='Total number of seconds to run for')
+    parser.add_argument('--batch-size',required=True,type=int,help='Number of documents to insert per batch')
+    parser.add_argument('--rate-limit',required=False,type=int,default=9999999,help='Limit throughput (operations per second)')
+    parser.add_argument('--text-size',required=False,type=int,default=1024,help='Size of text field (bytes)')
+    parser.add_argument('--text-compressible',required=False,type=int,default=25,help='Compressibility of text field (percentage)')
+    parser.add_argument('--ordered-batches',required=False,action='store_true',help='Use ordered bulk-writes')
+    parser.add_argument('--drop-collection',required=False,action='store_true',help='Drop the collection (if it exists)')
+    parser.add_argument('--compress',required=False,action='store_true',help='Compress the collection')
+    parser.add_argument('--shard',required=False,action='store_true',help='Shard the collection')
+    parser.add_argument('--num-customers',required=False,type=int,default=10000,help='Number of customers')
+    parser.add_argument('--num-products',required=False,type=int,default=1000000,help='Number of products')
+    parser.add_argument('--max-quantity',required=False,type=int,default=100000,help='Maximum quantity')
+    parser.add_argument('--seconds-date-range',required=False,type=int,default=87400*90,help='Number of seconds for range of orderDate field')
+    parser.add_argument('--num-secondary-indexes',required=False,type=int,default=3,choices=[0,1,2,3],help='Number of secondary indexes')
+    parser.add_argument('--file-name',required=False,type=str,default='benchmark',help='Starting name of the created CSV and log files')
+
+    args = parser.parse_args()
+    
+    if args.compress and not args.drop_collection:
+        printLog("Collection must be dropped to enable compression, add --drop-collection",appConfig)
+        sys.exit(1)
+
+    if args.shard and not args.drop_collection:
+        printLog("Collection must be dropped to enable sharding, add --drop-collection",appConfig)
+        sys.exit(1)
+
+    appConfig = {}
+    appConfig['uri'] = args.uri
+    appConfig['numInsertProcesses'] = int(args.processes)
+    appConfig['databaseName'] = args.database
+    appConfig['collectionName'] = args.collection
+    appConfig['runSeconds'] = int(args.run_seconds)
+    appConfig['rateLimit'] = int(args.rate_limit)
+    appConfig['batchSize'] = int(args.batch_size)
+    appConfig['textSize'] = int(args.text_size)
+    appConfig['textCompressible'] = int(args.text_compressible)
+    appConfig['orderedBatches'] = args.ordered_batches
+    appConfig['dropCollection'] = args.drop_collection
+    appConfig['compress'] = args.compress
+    appConfig['shard'] = args.shard
+    appConfig['numCustomers'] = int(args.num_customers)
+    appConfig['numProducts'] = int(args.num_products)
+    appConfig['maxQuantity'] = int(args.max_quantity)
+    appConfig['secondsDateRange'] = int(args.seconds_date_range)
+    appConfig['numSecondaryIndexes'] = int(args.num_secondary_indexes)
+    appConfig['logFileName'] = "{}.log".format(args.file_name)
+    appConfig['csvFileName'] = "{}.csv".format(args.file_name)
+    
+    numExistingDocuments = 0
+
+    random.seed()
+    numExistingDocuments = setup(appConfig)
+    
+    appConfig['numExistingDocuments'] = numExistingDocuments
+    
+    deleteLog(appConfig)
+    deleteCsv(appConfig)
+    
+    printLog('---------------------------------------------------------------------------------------',appConfig)
+    for thisKey in appConfig:
+        if (thisKey == 'uri'):
+            thisUri = appConfig[thisKey]
+            thisParsedUri = pymongo.uri_parser.parse_uri(thisUri)
+            thisUsername = thisParsedUri['username']
+            thisPassword = thisParsedUri['password']
+            thisUri = thisUri.replace(thisUsername,'<USERNAME>')
+            thisUri = thisUri.replace(thisPassword,'<PASSWORD>')
+            printLog("  config | {} | {}".format(thisKey,thisUri),appConfig)
+        else:
+            printLog("  config | {} | {}".format(thisKey,appConfig[thisKey]),appConfig)
+    printLog('---------------------------------------------------------------------------------------',appConfig)
+    
+    mp.set_start_method('spawn')
+    q = mp.Manager().Queue()
+    t = threading.Thread(target=reporter,args=(q,appConfig,))
+    t.start()
+    
+    processList = []
+    for loop in range(appConfig['numInsertProcesses']):
+        #time.sleep(1)
+        p = mp.Process(target=task_worker,args=(loop,q,appConfig))
+        processList.append(p)
+        
+    for process in processList:
+        process.start()
+        
+    for process in processList:
+        process.join()
+        
+    t.join()
+    
+    reportCollectionInfo(appConfig)
+    
+    printLog("Created {} and {} with results".format(appConfig['logFileName'],appConfig['csvFileName']),appConfig)
+
+
+if __name__ == "__main__":
+    main()
